ad1ecadc56c7f4ba0c4af2a1adf86b666a60e802
VikingErikBloodaxe <96142684+VikingErikBloodaxe@users.noreply.github.com>
2026-09-07 19:17:01 +0100

    Fix issue with photo mapping once added through site

--- Program.cs
+++ Program.cs
@@ -33,8 +33,10 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseRouting();
 
+app.UseStaticFiles();
+
+app.UseRouting();
 app.UseAuthorization();
 app.UseAuthorization();
 
