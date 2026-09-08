27c7cfb9dfbc75371632b9e79402c79d0386b34b
VikingErikBloodaxe <96142684+VikingErikBloodaxe@users.noreply.github.com>
2026-09-08 06:56:11 +0100

    Add victim year of death and refine case layout

---
+++ Migrations/CrimeDb/20260907194012_AddPersonYearOfDeath.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UKCrimeWeb.Migrations.CrimeDb
+{
+    /// <inheritdoc />
+    public partial class AddPersonYearOfDeath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "YearOfDeath",
+                table: "Person",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "YearOfDeath",
+                table: "Person");
+        }
+    }
+}
--- Models/Person.cs
+++ Models/Person.cs
@@ -9,6 +9,7 @@ namespace UKCrimeWeb.Models
         public string? LastName { get; set; }
 
         public string? Nickname { get; set; }
+        public int? YearOfDeath { get; set; }
 
         public int? YearStarted { get; set; }
 
