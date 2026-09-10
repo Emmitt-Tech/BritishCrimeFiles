822619794d55a707ca338ce2e3a77c234e096e4b
Emmitt-Tech <96142684+Emmitt-Tech@users.noreply.github.com>
2026-09-10 07:14:48 +0100

    Sync Book model with database fields

--- Models/Book.cs
+++ Models/Book.cs
@@ -3,8 +3,12 @@ namespace UKCrimeWeb.Models
     public class Book
     {
         public int BookId { get; set; }
-        public string Title { get; set; }
-        public string? Author { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public int? YearPublished { get; set; }
+        public string? ISBN { get; set; }
+        public bool? InPrint { get; set; }
+        public decimal? GoodreadsRating { get; set; }
         public string? PurchaseUrl { get; set; }
         public string? ImageUrl { get; set; }
         public ICollection<PersonBook> PersonBooks { get; set; } = new List<PersonBook>();
