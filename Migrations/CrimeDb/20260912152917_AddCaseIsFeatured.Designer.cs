130f562dd646aec29dfc52b0f4e2482b93d36f28
VikingErikBloodaxe <96142684+VikingErikBloodaxe@users.noreply.github.com>
2026-09-12 16:44:28 +0100

    Add featured case migration

--- Migrations/CrimeDb/20260912152917_AddCaseIsFeatured.Designer.cs
+++ Migrations/CrimeDb/20260912152917_AddCaseIsFeatured.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UKCrimeWeb.Models;
 
@@ -11,9 +12,11 @@ using UKCrimeWeb.Models;
 namespace UKCrimeWeb.Migrations.CrimeDb
 {
     [DbContext(typeof(CrimeDbContext))]
-    partial class CrimeDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260912152917_AddCaseIsFeatured")]
+    partial class AddCaseIsFeatured
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/CrimeDb/20260912152917_AddCaseIsFeatured.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UKCrimeWeb.Migrations.CrimeDb
+{
+    /// <inheritdoc />
+    public partial class AddCaseIsFeatured : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFeatured",
+                table: "Case",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFeatured",
+                table: "Case");
+        }
+    }
+}
